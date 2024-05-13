05da15c3935852e14ef15e0a7dae990ab8268fa5
yuvalTrip <77538019+yuvalTrip@users.noreply.github.com>
2024-05-14 00:25:51 +0300

    3b mincover.py, test_mincover function almost finished

--- mincover.py
+++ mincover.py
@@ -2,6 +2,7 @@ import subprocess, sys
 import time
 import numpy as np
 import cvxopt.glpk
+from networkx.algorithms.approximation import min_weighted_vertex_cover
 
 subprocess.check_call([sys.executable, "-m", "pip", "install", "cvxpy"], stdout=subprocess.DEVNULL)
 subprocess.check_call([sys.executable, "-m", "pip", "install", "cvxopt"], stdout=subprocess.DEVNULL)
@@ -34,14 +35,15 @@ def test_mincover():
         # Generate a random Erdos-Renyi graph
         graph_er = nx.erdos_renyi_graph(size, 0.2)
         result_er = mincover(graph_er)
+       # result_networkx= min_weighted_vertex_cover(graph_er)
         print(
             f"Erdos-Renyi Graph Size: {size}x{size}, Minimum Vertex Cover: {result_er}")
-        assert result_er <= size, f"Test failed for Erdos-Renyi graph size {size}x{size}: Vertex cover size greater than number of nodes."
+        assert result_er <= size, f"Test failed for graph size {size}x{size}: Vertex cover size greater than number of nodes."
         # Find the minimum weighted vertex cover
-        min_vertex_cover = nx.algorithms.approximation.vertex_cover.min_weighted_vertex_cover(graph_er)
+        min_vertex_cover = min_weighted_vertex_cover(graph_er)
         # Calculate the size of the minimum vertex cover
         min_vertex_cover_size = len(min_vertex_cover)
-        assert result_er == min_vertex_cover_size, f"Test failed for Erdos-Renyi graph size {size}x{size}: the real VC size is {min_vertex_cover_size}."
+        assert result_er <= min_vertex_cover_size, f"Test failed for Erdos-Renyi graph size {size}x{size}: the real VC size is {min_vertex_cover_size}."
 
         # Generate a random Barabasi-Albert graph
         graph_ba = nx.barabasi_albert_graph(size, 3)
@@ -50,10 +52,10 @@ def test_mincover():
             f"Barabasi-Albert Graph Size: {size}x{size}, Minimum Vertex Cover: {result_ba}")
         assert result_ba <= size, f"Test failed for Barabasi-Albert graph size {size}x{size}: Vertex cover size greater than number of nodes."
         # Find the minimum weighted vertex cover
-        min_vertex_cover = nx.algorithms.approximation.vertex_cover.min_weighted_vertex_cover(graph_er)
+        min_vertex_cover = min_weighted_vertex_cover(graph_er)
         # Calculate the size of the minimum vertex cover
         min_vertex_cover_size = len(min_vertex_cover)
-        assert result_ba == min_vertex_cover_size, f"Test failed for Barabasi-Albert graph size {size}x{size}: the real VC size is {min_vertex_cover_size}."
+        assert result_ba <= min_vertex_cover_size, f"Test failed for Barabasi-Albert graph size {size}x{size}: the real VC size is {min_vertex_cover_size}."
 
         # Generate a random Watts-Strogatz graph
         graph_ws = nx.watts_strogatz_graph(size, 4, 0.3)
@@ -62,15 +64,15 @@ def test_mincover():
             f"Watts-Strogatz Graph Size: {size}x{size}, Minimum Vertex Cover: {result_ws}")
         assert result_ws <= size, f"Test failed for Watts-Strogatz graph size {size}x{size}: Vertex cover size greater than number of nodes."
         # Find the minimum weighted vertex cover
-        min_vertex_cover = nx.algorithms.approximation.vertex_cover.min_weighted_vertex_cover(graph_er)
+        min_vertex_cover = min_weighted_vertex_cover(graph_er)
         # Calculate the size of the minimum vertex cover
         min_vertex_cover_size = len(min_vertex_cover)
-        assert result_ws == min_vertex_cover_size, f"Test failed for Watts-Strogatz graph size {size}x{size}: the real VC size is {min_vertex_cover_size}."
+        assert result_ws <= min_vertex_cover_size, f"Test failed for Watts-Strogatz graph size {size}x{size}: the real VC size is {min_vertex_cover_size}."
 
 
 if __name__ == '__main__':
-    print(cp.installed_solvers())
-    edges=eval(input())
-    graph = nx.Graph(edges)
-    print(mincover((graph)))
-    # test_mincover()
+#    print(cp.installed_solvers()) #internal checking because bug
+   # edges=eval(input())
+    #graph = nx.Graph(edges)
+    #print(mincover((graph)))
+    test_mincover()
